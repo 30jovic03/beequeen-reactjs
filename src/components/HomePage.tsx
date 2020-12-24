7890dc16e64c8d2e4316d406ee0f6df2161d51c6
Mladen Jovic <30jovic03@gmail.com>
2020-12-24 21:22:21 +0100

    Edit HomePage component

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,20 +1,80 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
+import { faListAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CategoryType from '../types/CategoryType';
+import { Link } from 'react-router-dom';
+import { projectFirestore } from '../firebase/config';
 import MainMenu from './MainMenu';
 
+interface HomePageState {
+  categories: CategoryType[];
+}
 
 class HomePage extends React.Component {
+  state: HomePageState;
+
+  constructor(props: Readonly<{}>) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  private getCategories() {
+    projectFirestore.collection("categories").where("parentCategoryId", "==", "").get().then((querySnapshot) => {
+      let documents: CategoryType[] = [];
+      querySnapshot.forEach(doc => {
+        documents.push({...doc.data(), categoryId: doc.id});
+      });
+
+      this.putCategoriesInState(documents);
+    });
+  }
+
+  private putCategoriesInState(data: CategoryType[]) {
+    this.setState(Object.assign(this.state, {
+      categories: data,
+    }));
+  }
 
   render() {
-    return(
+    return (
       <Container>
         <MainMenu role='user' />
 
-        <div>
-          Home page
-        </div>
+        <Card>
+          <Card.Header className="bg-warning">
+            <Card.Title>
+              <FontAwesomeIcon icon={ faListAlt } /> Kategorije proizvoda
+            </Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <ul id="grid" className="clear">
+              { this.state.categories.map(this.singleCategory) }
+            </ul>
+          </Card.Body>
+        </Card>
       </Container>
-    )
+    );
+  }
+
+  private singleCategory(category: CategoryType) {
+    return (
+      <li>
+        <Link to={ `/category/${ category.categoryId }` }>
+          <div className="hexagon">
+            <img src={ category.imageUrl } />
+            <p>{ category.name }</p>
+          </div>
+        </Link>
+      </li>
+    );
   }
 }
 
